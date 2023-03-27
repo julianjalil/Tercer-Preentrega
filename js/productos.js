7cb096f01becaf7789e41e7b47b0e3896daaf081
julianjalil <juliaanjalil@gmail.com>
2023-03-27 20:44:33 -0300

    primerCommit

---
+++ js/productos.js
@@ -0,0 +1,29 @@
+const productos = [{
+        id: 1,
+        nombre: "Camperita de hilo de algodon",
+        precio: 3700,
+        img: "img/camperaHilo.jpg",
+        cantidad: 1,
+    },
+    {
+        id: 2,
+        nombre: "Babucha de algodon",
+        precio: 2000,
+        img: "img/pantAlgodon.jpg",
+        cantidad: 1,
+    },
+    {
+        id: 3,
+        nombre: "Ranita de algodon",
+        precio: 2100,
+        img: "img/ranitaAlgodon.jpg",
+        cantidad: 1,
+    },
+    {
+        id: 4,
+        nombre: "Sueter hippie de algodon",
+        precio: 3300,
+        img: "img/sueterHippie.jpg",
+        cantidad: 1,
+    },
+];
